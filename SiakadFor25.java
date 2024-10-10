ef91c1ae49438f11b2d1e6d8bcc617a42bab0d16
wandi1209 <wandider@gmail.com>
2024-10-10 08:45:42 +0700

    P7_Percobaan1_PertanyaanNo5

--- SiakadFor25.java
+++ SiakadFor25.java
@@ -5,6 +5,7 @@ public class SiakadFor25 {
         // Deklarasi Variabel
         Scanner input = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0;
 
         // Perulangan Input Nilai
         for (int i = 1; i <= 10; i++) {
@@ -16,8 +17,11 @@ public class SiakadFor25 {
             if(nilai < terendah) {
                 terendah = nilai;
             }
+            lulus += (nilai >= 60) ? 1 : 0 ;
         }
         System.out.println("Nilai Tertinggi : " + tertinggi);
         System.out.println("Nilai Terendah : " + terendah);
+        System.out.println("Jumlah Mahasiswa Yang Lulus : " + lulus);
+        System.out.println("Jumlah Mahasiswa Yang Tidak Lulus : " + (10 - lulus));
     }
 }
